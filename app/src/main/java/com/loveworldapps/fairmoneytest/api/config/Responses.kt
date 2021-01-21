5993d2fbae07d008f9439225399b174bc8c9b535
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2021-01-21 17:28:35 +0100

    Response Class for Internet Issues

---
+++ app/src/main/java/com/loveworldapps/fairmoneytest/api/config/Responses.kt
@@ -0,0 +1,34 @@
+package com.loveworldapps.fairmoneytest.api.config
+
+import com.loveworldapps.fairmoneytest.Constants
+import okhttp3.*
+
+/**
+ * Created by manuelchris-ogar on 11/01/2021.
+ */
+class Responses  {
+
+    companion object{
+        val TYPE_JSON = MediaType.parse("application/json")
+        val NO_INTERNET = "{\"status\": \"error\"," +
+                "\"code\": \"" + "No Internet"+ "\"," +
+                "\"message\": \"Please check your internet connection and try again.\"," +
+                "\"reason\" : \"No Internet Connection\"}"
+
+        fun noInternet(chain:Interceptor.Chain): Response{
+
+
+            return Response.Builder()
+                    .request(chain.request())
+                    .protocol(Protocol.HTTP_1_1)
+                    .code(Constants.NO_INTERNET)
+                    .message("Connection Error")
+                    .body(ResponseBody.create(TYPE_JSON, NO_INTERNET))
+                    .sentRequestAtMillis(-1L)
+                    .receivedResponseAtMillis(System.currentTimeMillis())
+                    .build()
+        }
+
+
+    }
+}
