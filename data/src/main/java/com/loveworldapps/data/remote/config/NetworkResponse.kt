2918a3ad9b73897e2fb7e92e7de817a4a9646a9a
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2021-01-22 09:01:42 +0100

    Moved API Config Settings to Data Layer

---
+++ data/src/main/java/com/loveworldapps/data/remote/config/NetworkResponse.kt
@@ -0,0 +1,42 @@
+package com.loveworldapps.data.remote.config
+
+import com.loveworldapps.domain.model.base.BaseResponse
+
+
+/**
+ * Created by manuelchris-ogar on 20/01/2021.
+ */
+
+public open class NetworkResponse <out T : Any, out U : Any>: BaseResponse() {
+    /**
+     * Success response with body
+     */
+    data class Success<T : Any>(val body: T) : NetworkResponse<T, Nothing>()
+
+    /**
+     * Failure response with body
+     */
+    data class ApiError<U : Any>(val body: String, val code: String) : NetworkResponse<Nothing, U>()
+
+    /**
+     * No Intenet
+     */
+    object  NoInternet : NetworkResponse<Nothing, Nothing>()
+
+
+    /**
+     * Network error
+     */
+    data class NetworkError(val error: Exception) : NetworkResponse<Nothing, Nothing>()
+
+    /**
+     * For example, json parsing error
+     */
+    data class UnknownError(val error: Throwable?) : NetworkResponse<Nothing, Nothing>()
+
+
+    /**
+     * User's token is invalid, show error
+     */
+    object TokenInvalid : NetworkResponse<Nothing, Nothing>()
+}
